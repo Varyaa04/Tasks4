10f649418224019800710147d3ab04ac86161409
Varushka ☆彡 <fedorova04_varya@mail.ru>
2023-10-22 17:12:57 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun areCoprime(m: Int, n: Int): Boolean {
+    var a = m
+    var b = n
+
+    while (b != 0) {
+        val temp = b
+        b = a % b
+        a = temp
+    }
+
+    return a == 1
+}
+
+fun main() {
+    println("Введите два числа:")
+    val m = readLine()?.toIntOrNull()
+    val n = readLine()?.toIntOrNull()
+
+    if (m == null || n == null) {
+        println("Некорректный ввод")
+        return
+    }
+
+    val coprime = areCoprime(m, n)
+
+    if (coprime) {
+        println("Числа $m и $n являются взаимно простыми")
+    } else {
+        println("Числа $m и $n не являются взаимно простыми")
+    }
+}
